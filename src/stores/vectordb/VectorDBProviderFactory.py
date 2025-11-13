205f156d162eb749ddb98478791f62c5d627034e
omarsabri125 <omarsabri8756@gmail.com>
2025-11-13 16:02:59 +0200

    update factory

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,12 +1,15 @@
 from .providers import QdrantDBProvider
+from .providers import PGVectorProvider
 from .VectorDBEnums import VectorDBEnums
 from controllers.BaseController import BaseController
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBProviderFactory:
 
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, db_client: sessionmaker = None):
         self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
     
     def create(self, provider: str):
 
@@ -14,8 +17,17 @@ class VectorDBProviderFactory:
             qdrant_db_client = self.base_controller.get_database_path(db_name = self.config.QDRANT_DB_PATH)
 
             return QdrantDBProvider(
-                db_path = qdrant_db_client,
-                distance_method = self.config.QDRANT_DISTANCE_METHOD
+                db_client = qdrant_db_client,
+                distance_method = self.config.VECTOR_DB_DISTANCE_METHOD
+            )
+        
+        if provider == VectorDBEnums.PGVECTOR.value:
+
+            return PGVectorProvider(
+                db_client = self.db_client,
+                default_vector_size = self.config.EMBEDDING_MODEL_DIMENSION,
+                distance_method = self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold = self.config.INDEX_THRESHOLD
             )
         
         return None
