899a32c3266cd54abd2c75027f8cb68198f08fe5
omarsabri125 <omarsabri8756@gmail.com>
2025-12-08 21:28:52 +0200

    Add CACHE_ANSWER_ERROR and CACHE_ANSWER_SUCCESS to ResponseEnumeration

--- src/models/enums/ResponseEnumeration.py
+++ src/models/enums/ResponseEnumeration.py
@@ -15,6 +15,8 @@ class ResponseEnumeration(Enum):
     VECTORDB_SEARCH_SUCCESS = "vectordb_search_success"
     RAG_ANSWER_ERROR = "rag_answer_error"
     RAG_ANSWER_SUCCESS = "rag_answer_success"
+    CACHE_ANSWER_ERROR = "cache_answer_error"
+    CACHE_ANSWER_SUCCESS = "cache_answer_success"
     QUERY_EXPANSION_FAILD = "query_expansion_faild"
     # PROCESSING_SUCCESS = "File {file_name} processed successfully with {chunks} chunks."
     PROCESSING_SUCCESS = "processing_success"
